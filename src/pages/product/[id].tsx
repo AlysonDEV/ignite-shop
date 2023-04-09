c60db5c1bcbf9288fd98ff411eff28c320046d6f
AlysonDev <devalyson@gmail.com>
2023-04-09 06:24:01 -0300

    Carregados os produtos dentro da página.

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,21 +1,62 @@
+import { stripeAPI } from "@/lib/stripe"
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import { GetStaticProps } from "next"
+import Image from "next/image"
 import { useRouter } from "next/router"
+import Stripe from "stripe"
 
-export default function Products () {
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
+
+export default function Products ({product}: ProductProps) {
   const { query } = useRouter()
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageUrl} alt={product.name} width={520} height={480} />
       </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, laudantium. Rem facere dolores autem porro, maiores assumenda esse officia temporibus quasi delectus magnam, amet maxime quos ipsam cupiditate, consequatur asperiores.</p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>
           Comprar agora
         </button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripeAPI.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price;
+
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format((price.unit_amount ?? 0) / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1, // 2 horas
+  }
 }
