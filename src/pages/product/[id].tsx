0e95423d8f7ee758d79206089fae20adc0aff616
AlysonDev <devalyson@gmail.com>
2023-04-09 05:52:42 -0300

    Terminado a estrutura do HTML para paginda Produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Products () {
   const { query } = useRouter()
   return (
-    <h1>Products {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, laudantium. Rem facere dolores autem porro, maiores assumenda esse officia temporibus quasi delectus magnam, amet maxime quos ipsam cupiditate, consequatur asperiores.</p>
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
